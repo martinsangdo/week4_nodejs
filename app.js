4440500b5c89c01aea7d72ff7b3add448cf70157
sangdo <sang.do@knorex.com>
2024-06-05 12:07:30 +0700

    - add functions

--- app.js
+++ app.js
@@ -9,6 +9,36 @@ app.get('/', (req, res) => {
 app.get('/test1', (req, res) => {
   res.send('Hello ' + req.query.name);
 });
+//Exercise 1
+app.get('/pagination', (req, res) => {
+  var current_page = req.query.index;
+  var numbers = [];
+  //todo: highlight the index number
+  for (var i=1; i<10; i++){
+    numbers.push('<strong style="text-decoration: underline;">' + i + '</strong>');
+  }
+  var string = numbers.join(" ");
+  res.send(string);
+});
+//Exercise 2
+app.get('/remove_duplication', (req, res) => {
+  var string = req.query.string;
+  var chars = string.split('');
+  console.log(chars);
+  //todo: remove duplicate chars, then join all back into a string
+  res.send(chars.join(''));
+});
+//Exercise 3
+app.get('/permutation', (req, res) => {
+  var string = req.query.string;
+  var chars = string.split('');
+  console.log(chars);
+  //todo: calculate all permuation of input string
+  /*
+  For ex: input 'ABC', print ABC”, “ACB”, “BAC”, “BCA”, “CBA”, “CAB”
+   */
+  res.send(chars.join(''));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
